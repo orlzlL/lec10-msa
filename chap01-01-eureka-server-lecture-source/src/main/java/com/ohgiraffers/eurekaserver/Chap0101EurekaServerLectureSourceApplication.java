db8e30dbcf2fc36e425478618d1b7e766a682202
orlzlL <yerincho06@gmail.com>
2024-02-29 15:13:57 +0900

    init

---
+++ chap01-01-eureka-server-lecture-source/src/main/java/com/ohgiraffers/eurekaserver/Chap0101EurekaServerLectureSourceApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap0101EurekaServerLectureSourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chap0101EurekaServerLectureSourceApplication.class, args);
+    }
+
+}
---
+++ chap01-02-eureka-client-lecture-source/src/main/java/com/ohgiraffers/eurekaclient/Chap0102EurekaClientLectureSourceApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.eurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap0102EurekaClientLectureSourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chap0102EurekaClientLectureSourceApplication.class, args);
+    }
+
+}
