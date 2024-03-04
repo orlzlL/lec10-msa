90f21d29e210ea67224f381c812f88a2132815cf
orlzlL <yerincho06@gmail.com>
2024-03-04 12:31:15 +0900

    로드밸런싱

--- chap02-second-service-lecture-source/src/main/java/com/ohgiraffers/secondservice/Chap02SecondServiceLectureSourceApplication.java
+++ chap02-second-service-lecture-source/src/main/java/com/ohgiraffers/secondservice/Chap02SecondServiceLectureSourceApplication.java
@@ -9,5 +9,4 @@ public class Chap02SecondServiceLectureSourceApplication {
     public static void main(String[] args) {
         SpringApplication.run(Chap02SecondServiceLectureSourceApplication.class, args);
     }
-
 }
