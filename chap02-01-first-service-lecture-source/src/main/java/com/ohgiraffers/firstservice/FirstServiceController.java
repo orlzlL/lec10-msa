b4ba4eeaedd1cd41052e5cc621717141fd9e992c
orlzlL <yerincho06@gmail.com>
2024-03-04 10:51:57 +0900

    gateway

---
+++ chap02-01-first-service-lecture-source/src/main/java/com/ohgiraffers/firstservice/FirstServiceController.java
@@ -0,0 +1,23 @@
+package com.ohgiraffers.firstservice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequestMapping("/first-service")
+public class FirstServiceController {
+
+    @GetMapping("health_check")
+    public String healthCheck(){
+        return "I'm OK";
+    }
+
+    @GetMapping("message")
+    public String message(@RequestHeader("first-request") String header){
+        return "First Service Message";
+    }
+}
---
+++ chap02-second-service-lecture-source/src/main/java/com/ohgiraffers/secondservice/Chap02SecondServiceLectureSourceApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.secondservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap02SecondServiceLectureSourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chap02SecondServiceLectureSourceApplication.class, args);
+    }
+
+}
