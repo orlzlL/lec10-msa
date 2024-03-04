482bc65d72e67bdcc1c6c4376d2c1cb5a12cac5d
orlzlL <yerincho06@gmail.com>
2024-03-04 09:43:42 +0900

    firsetService

---
+++ chap02-01-first-service-lecture-source/src/main/java/com/ohgiraffers/firstservice/Chap0201FirstServiceLectureSourceApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.firstservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap0201FirstServiceLectureSourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chap0201FirstServiceLectureSourceApplication.class, args);
+    }
+
+}
